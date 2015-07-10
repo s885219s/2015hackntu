e327c58c42032d63bc6c046f8a268a2b78012713
Arick Chen <s885219s@gmail.com>
2015-07-10 18:11:34 +0800

    fifth

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 /*
-	Directive by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+ Directive by HTML5 UP
+ html5up.net | @n33co
+ Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+ */
 
 $(function() {
-    $( "#tabs" ).tabs({
-      event: "mouseover"
-    });
-  });
+	$( "#tabs" ).tabs({
+		event: "mouseover"
+	});
+});
 
 function hrefOnClick(hash, e){
 	e.preventDefault();
@@ -25,17 +25,21 @@ function hrefOnClick(hash, e){
 (function($) {
 
 	$(function () {
-	  $(window).scroll(function () {
-	    console.log('here');
-		var $nav_bar = $( "#sub_main" );
-		var num = $(this).scrollTop();
-		if(  num >= 15 )
-			$nav_bar.addClass('change_background');
-		else
-			$nav_bar.removeClass('change_background');
-		console.log(num);
-	  	});
+		$(window).scroll(function () {
+			console.log('here');
+			var $nav_bar = $( "#sub_main" );
+			var num = $(this).scrollTop();
+			if(  num >= 15 )
+				$nav_bar.addClass('change_background');
+			else
+				$nav_bar.removeClass('change_background');
+			console.log(num);
+		});
 	});
+
+
+
+
 
 	skel.breakpoints({
 		wide: '(max-width: 1680px)',
@@ -46,34 +50,30 @@ function hrefOnClick(hash, e){
 		mobilep: '(max-width: 480px)'
 	});
 
-	$("#apply_button").click(function(){
-		window.location.href = "http://www.accupass.com/go/2015hackntu";
-	});
-
 	$(function() {
 
-		
+
 
 		var	$window = $(window),
 			$body = $('body');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+		$window.on('load', function() {
+			$body.removeClass('is-loading');
+		});
 
 		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+		$('form').placeholder();
 
 		// Prioritize "important" elements on narrower.
-			skel.on('+narrower -narrower', function() {
-				$.prioritize(
-					'.important\\28 narrower\\29',
-					skel.breakpoint('narrower').active
-				);
-			});
+		skel.on('+narrower -narrower', function() {
+			$.prioritize(
+				'.important\\28 narrower\\29',
+				skel.breakpoint('narrower').active
+			);
+		});
 
 	});
 
